260738c09be72201894e6e4bdbfaea7c463e620b
nia-hamilton <nia.hamilton@stageslearning.com>
2021-12-23 16:43:31 -0500

    Added PHP functionality to front page and default page

---
+++ page.php
@@ -0,0 +1,12 @@
+<section class="main">
+  <section class="graphics">
+    <div class="graphic cyan"></div>
+    <div class="graphic green"></div>
+    <div class="graphic yellow"></div>
+    <div class="graphic red"></div>
+  </section>
+  <section class="heading-text">
+    <h1><?php the_title();?></h1>
+    <p><?php the_content();?></p>
+  </section>
+</section>
