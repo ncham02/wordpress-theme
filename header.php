fd728ae7c05297abc9c8c722c8aa8bbef91bc3fe
nia-hamilton <nia.hamilton@stageslearning.com>
2022-01-03 20:21:11 -0500

    Updated viewport settings

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head();?>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body <?php body_class();?>>
 
